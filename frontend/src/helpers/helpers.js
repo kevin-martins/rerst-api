74da4d7bfbf5230b2e2013f3e4255f91c4377a6f
Kevin <kev.martinsvieira@gmail.com>
2023-11-13 01:14:33 +0100

    feat: add date management function in helper

---
+++ frontend/src/helpers/helpers.js
@@ -0,0 +1,17 @@
+export const capitalize = (string) => {
+  return string.substring(0, 1).toUpperCase() + string.substring(1, string.length);
+}
+
+export const changeDate = (date) => {
+  const options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+  }
+  const [day, dateTime, month, ...others] = new Date(date).toLocaleDateString('fr-FR', options).split(' ');
+  return capitalize(day) + ' ' + dateTime + ' ' + capitalize(month) + ' ' + others.join(' ');
+}
