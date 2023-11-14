88116f0d781e64c1c69c87fd3edad0603652c4de
kevin-martins <kev.martinsvieira@gmail.com>
2023-11-14 18:49:26 +0100

    feat: page to display every places

--- frontend/src/pages/Places.jsx
+++ frontend/src/pages/Places.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
+import Loading from '../components/Loading';
 
 const Places = () => {
+  const [isLoading, setIsLoading] = useState(true)
+  const [places, setPlaces] = useState([])
+  useEffect(() => {
+    fetch('http://localhost:4000/places')
+    .then((res) => res.json())
+      .then((res) => {
+        setPlaces(res);
+        setIsLoading(false)
+      })
+      .catch((error) => console.log(error));
+  })
   return (
-    <div>Places</div>
+    <ul className='flex flex-wrap gap-5'>
+      {isLoading
+        ? <Loading />
+        : places.map(place => (
+        <li className='w-96 h-44 bg-slate-900 mx-auto text-white rounded-lg border-2 border-slate-900 hover:border-yellow-600'>
+          <Link
+            to={`/places/${place._id}`}
+            className='inline-block w-full h-full grid place-items-center'
+          >
+            Voir la place
+          </Link>
+        </li>
+      ))}
+    </ul>
   )
 }
 
