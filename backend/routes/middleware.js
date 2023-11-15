dde2b99df594ad96e3c732955121252eb3a0fcdd
kevin-martins <kev.martinsvieira@gmail.com>
2023-11-15 14:57:04 +0100

    fix: update error return type

--- backend/routes/middleware.js
+++ backend/routes/middleware.js
@@ -1,19 +1,17 @@
 const bcrypt = require('bcrypt');
-const User = require('./models/User'); // Import your User model
+const User = require('../models/User');
 
-const auth = async (req, res, next) => {
+exports.auth = async (req, res, next) => {
   try {
     const { phone_number, password } = req.body;
     const user = await User.findOne({ phone_number });
 
     if (!user || !(await bcrypt.compare(password, user.password))) {
-      return res.status(401).send('Error: invalid phone number or password');
+      return res.status(401).json({ message: 'Error: invalid phone number or password' });
     }
 
     next();
-  } catch (error) {
-    res.status(500).send('Internal Server Error');
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
-};
-
-module.exports = auth
+}
