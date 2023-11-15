f0dbd1d528709158d484c0abebcfcaaa260e96eb
kevin-martins <kev.martinsvieira@gmail.com>
2023-11-15 12:49:16 +0100

    feat: add password schama prop and update min error message

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,12 +4,12 @@ const userSchema = new mongoose.Schema({
   pass_id: String,
   first_name: {
     type: String,
-    min: [2, "Error: first name should be at least of size 2"],
+    min: [2, "Error: first name should at least be of size 2"],
     required: [true, "Error: first name is required"],
   },
   last_name: {
     type: String,
-    min: [2, "Error: last name should be at least of size 2"],
+    min: [2, "Error: last name should at least be of size 2"],
     required: [true, "Error: last name is required"],
   },
   age: {
@@ -20,6 +20,11 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, "Error: phone number is required"],
     unique: true
+  },
+  password: {
+    type: String,
+    required: [true, "Error: password is required"],
+    min: [8, "Error: password should at least be of size 8"]
   },
   address: String
 });
