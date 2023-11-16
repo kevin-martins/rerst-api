5c4dde4bdbde612b5d6bd82258d2f872b7bbc9ff
kevin-martins <kev.martinsvieira@gmail.com>
2023-11-16 23:09:39 +0100

    feat: make a commun card for place and user

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,18 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const UserCard = (props) => {
-  console.log(props._id)
+const Card = ({ link, text }) => {
   return (
     <li className='w-96 h-44 bg-slate-900 mx-auto text-white rounded-lg border-2 border-slate-900 hover:border-yellow-600'>
       <Link
-        to={`/users/${props._id}`}
+        to={link}
         className='inline-block w-full h-full grid place-items-center'
       >
-        {props.first_name}{' '}{props.last_name}
+        {text}
       </Link>
     </li>
   )
 }
 
-export default UserCard
+export default Card
