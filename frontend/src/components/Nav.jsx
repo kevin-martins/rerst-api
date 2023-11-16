530e148e37fdd43c6e3c0e3eb8d01dda49cde61a
kevin-martins <kev.martinsvieira@gmail.com>
2023-11-16 22:33:01 +0100

    feat: logoff button in the navbar

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const Nav = () => {
+const Nav = ({ setIsLogged }) => {
 	const navigation = [
 		{ title: "Accueil", href: "/" },
 		{ title: "Utilisateurs", href: "/users" },
 		{ title: "Places", href: "/places" },
 	]
+
+  const handleClick = () => {
+    setIsLogged(false)
+  }
+
   return (
     <nav className='max-w-5xl bg-slate-900 mx-auto p-3 shadow-lg rounded-lg text-white'>
 			<ul className='flex gap-4'>
@@ -22,6 +27,7 @@ const Nav = () => {
 						</Link>
 				</li>
 				))}
+        <button className='ml-auto px-3' onClick={handleClick}>Log off</button>
 			</ul>
     </nav>
   )
