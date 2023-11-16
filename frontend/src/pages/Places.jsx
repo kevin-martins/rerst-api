55ba96b1da11f23d54f2bd58724045b6f87379cf
kevin-martins <kev.martinsvieira@gmail.com>
2023-11-16 23:10:35 +0100

    feat: display cards with the card component

--- frontend/src/pages/Places.jsx
+++ frontend/src/pages/Places.jsx
@@ -1,34 +1,35 @@
 import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom';
 import Loading from '../components/Loading';
+import Card from '../components/Card';
 
 const Places = () => {
   const [isLoading, setIsLoading] = useState(true)
   const [places, setPlaces] = useState([])
   useEffect(() => {
     fetch('http://localhost:8080/places')
-    .then((res) => res.json())
+      .then((res) => res.json())
       .then((res) => {
+        console.log(res)
         setPlaces(res);
         setIsLoading(false)
       })
       .catch((error) => console.log(error));
   })
   return (
-    <ul className='flex flex-wrap gap-5'>
+    <>
       {isLoading
         ? <Loading />
-        : places.map(place => (
-        <li className='w-96 h-44 bg-slate-900 mx-auto text-white rounded-lg border-2 border-slate-900 hover:border-yellow-600'>
-          <Link
-            to={`/places/${place._id}`}
-            className='inline-block w-full h-full grid place-items-center'
-          >
-            Voir la place
-          </Link>
-        </li>
-      ))}
-    </ul>
+        : <ul className='flex flex-wrap gap-5'>
+            {places.map(place => (
+              <Card
+                key={place._id}
+                link={`/places/${place._id}`}
+                text="Voir la place"
+              />
+            ))}
+          </ul>
+        }
+    </>
   )
 }
 
