43a7f622d9a968ff085521d335515a5d2a082c7e
kevin-martins <kev.martinsvieira@gmail.com>
2023-11-16 23:11:12 +0100

    feat: display users with card components

--- frontend/src/pages/Users.jsx
+++ frontend/src/pages/Users.jsx
@@ -1,32 +1,35 @@
 import React, { useEffect, useState } from 'react'
 import Loading from '../components/Loading'
-import UserCard from '../components/UserCard'
+import Card from '../components/Card'
 
 const Users = () => {
   const [isLoading, setIsLoading] = useState(true)
-  const [data, setData] = useState([])
+  const [users, setUsers] = useState([])
 
   useEffect(() => {
     fetch(`http://localhost:8080/users`, {})
       .then((res) => res.json())
       .then((res) => {
-        console.log(res)
-        setData(res);
+        setUsers(res);
         setIsLoading(false);
       })
       .catch((error) => console.log(error));
   }, [])
   return (
-    <div className=''>
-      {isLoading 
+    <>
+      {isLoading
         ? <Loading />
         : <ul className='flex flex-wrap gap-2'>
-            {data.map(userData => (
-              <UserCard {...userData} />
+            {users.map(user => (
+              <Card
+                key={user._id}
+                link={`/users/${user._id}`}
+                text={`${user.first_name} ${user.last_name}`}
+              />
             ))}
           </ul>
       }
-    </div>
+    </>
   )
 }
 
