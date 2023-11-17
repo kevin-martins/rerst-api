b9097e8bccb6cdab7267773c86aa262176d12782
kevin-martins <kev.martinsvieira@gmail.com>
2023-11-17 11:21:04 +0100

    feat: export createUser to helper function

---
+++ backend/helpers/createUser.js
@@ -0,0 +1,31 @@
+const { User, Pass, Place } = require('../models');
+const bcrypt = require('bcrypt');
+
+const createUser = async (req, res) => {
+  try {
+    const pass = await Pass.create({ level: 1 });
+    if (!pass) {
+      throw new Error('Error: pass has not successfully been created');
+    }
+
+    const hashedPassword = await bcrypt.hash(req.body.password, 12);
+    if (!hashedPassword) {
+      throw new Error('Erorr: the hashed password has not successfully been created');
+    }
+
+    const user = await User.create({
+      ...req.body,
+      pass_id: pass._id,
+      password: hashedPassword
+    });
+    if (!user) {
+      throw new Error('Error: user has not successfully been created');
+    }
+
+    return user;
+  } catch (err) {
+    throw new Error(err.message);
+  }
+}
+
+module.exports = createUser;
