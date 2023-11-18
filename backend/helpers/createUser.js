fb7aef97b950ce505678f926427bed24cc231f27
Kevin <kev.martinsvieira@gmail.com>
2023-11-18 15:52:46 +0100

    fix: recupere correctement le body

--- backend/helpers/createUser.js
+++ backend/helpers/createUser.js
@@ -1,20 +1,20 @@
 const { User, Pass, Place } = require('../models');
 const bcrypt = require('bcrypt');
 
-const createUser = async (req, res) => {
+const createUser = async (body) => {
   try {
     const pass = await Pass.create({ level: 1 });
     if (!pass) {
       throw new Error('Error: pass has not successfully been created');
     }
 
-    const hashedPassword = await bcrypt.hash(req.body.password, 12);
+    const hashedPassword = await bcrypt.hash(body.password, 12);
     if (!hashedPassword) {
       throw new Error('Erorr: the hashed password has not successfully been created');
     }
 
     const user = await User.create({
-      ...req.body,
+      ...body,
       pass_id: pass._id,
       password: hashedPassword
     });
