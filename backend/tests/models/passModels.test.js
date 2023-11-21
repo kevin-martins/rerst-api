e3559d4e4180b391728dacfbcc6df2ea0ccddb97
Kevin <kev.martinsvieira@gmail.com>
2023-11-21 14:46:12 +0100

    test: check required fields and passLevel

---
+++ backend/tests/models/passModels.test.js
@@ -0,0 +1,57 @@
+const axios = require('axios');
+const { faker } = require('@faker-js/faker');
+const { addLocalPath } = require('../../helpers/helpers');
+
+describe("Pass models", () => {
+  it('should not create a new pass if the required fields is null, undefined or empty', async () => {
+    const passes = [
+      { level: null },
+      { level: undefined },
+      {}
+    ]
+    passes.forEach(async pass => {
+      const res = await axios
+        .post(addLocalPath('/passes'), pass)
+        .catch(err => err.response);
+  
+      expect(res.status).toBe(400);
+    })
+  });
+
+  it('should not create a new pass if level out 1 and 5 included', async () => {
+    const invalidLevels = [
+      faker.number.int({ min: 6, max: 10000 }),
+      faker.number.int({ min: -10000, max: 0 })
+    ];
+    invalidLevels.forEach(async level => {
+      const res = await axios
+        .post(addLocalPath('/passes'), { level })
+        .catch(err => err.response);
+
+      expect(res.status).toBe(400);
+    });
+  });
+
+  it('should not update a pass if level out 1 and 5 included', async () => {
+    const create = await axios
+      .post(addLocalPath('/passes'), {
+        level: faker.number.int({ min: 1, max: 5 })
+      });
+    expect(create.status).toBe(201);
+
+    const invalidLevels = [
+      faker.number.int({ min: 6, max: 10000 }),
+      faker.number.int({ min: -10000, max: 0 })
+    ];
+    invalidLevels.forEach(async level => {
+      const res = await axios
+        .put(addLocalPath(`/passes/${create.data._id}`), { level })
+        .catch(err => err.response);
+
+      expect(res.status).toBe(400);
+    });
+
+    const remove = await axios.delete(addLocalPath(`/passes/${create.data._id}`))
+    expect(remove.status).toBe(200);
+  });
+})
