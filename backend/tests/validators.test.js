ac0413003fcf835997675b40efd59433ee57e0bf
Kevin <kev.martinsvieira@gmail.com>
2023-11-21 14:45:42 +0100

    test: verify validators

--- backend/tests/validators.test.js
+++ backend/tests/validators.test.js
@@ -1,5 +1,11 @@
 const { faker } = require('@faker-js/faker');
-const { isLevelValid, isPlaceAccessValid, isAgeValid } = require("../helpers/validator");
+const {
+  isLevelValid,
+  isPlaceAccessValid,
+  isAgeValid,
+  isObjectKeysDefined,
+  isObjectKeysUniques
+ } = require("../helpers/validator");
 
 describe('Check Validators', () => {
   it('should return true if level between 1 and 5 included', async () => {
@@ -41,7 +47,7 @@ describe('Check Validators', () => {
     ];
     data.forEach(v => {
       expect(isPlaceAccessValid(v.userAge, v.placeAge, v.passLevel, v.placeLevel)).toBe(true);
-    })
+    });
   });
 
   it('should return false if user age or pass level is bellow the required place age and pass level', async () => {
@@ -61,7 +67,7 @@ describe('Check Validators', () => {
     ];
     data.forEach(v => {
       expect(isPlaceAccessValid(v.userAge, v.placeAge, v.passLevel, v.placeLevel)).toBe(false);
-    })
+    });
   });
 
   it('should return true if age in 18 and 150 included', async () => {
@@ -72,7 +78,7 @@ describe('Check Validators', () => {
     ];
     ages.forEach(age => {
       expect(isAgeValid(age)).toBe(true);
-    })
+    });
   });
 
   it('should return false if age out 18 and 150', async () => {
@@ -82,6 +88,69 @@ describe('Check Validators', () => {
     ];
     ages.forEach(age => {
       expect(isAgeValid(age)).toBe(false);
-    })
+    });
   });
+
+  it('should return true if a key is defined', async () => {
+    const obj = {
+      first_name : faker.person.firstName(),
+      last_name: faker.person.lastName()
+    };
+    expect(isObjectKeysDefined(obj, ["last_name", "first_name"])).toBe(true);
+    const objects = [
+      {
+        first_name: faker.person.firstName(),
+        last_name: faker.person.lastName(),
+        age: faker.number.int(),
+      },
+      {
+        age: faker.number.int(),
+        first_name: faker.person.firstName(),
+        last_name: faker.person.lastName(),
+      },
+      {
+        age: faker.person.lastName(),
+        last_name: faker.person.lastName(),
+        first_name: faker.person.firstName(),
+      },
+    ];
+    objects.forEach(object => {
+      expect(isObjectKeysDefined(object, ["last_name", "first_name", "age"])).toBe(true);
+    });
+  });
+
+  it('should return false if a key is undefined', async () => {
+    const objects = [
+      {
+        first_name: faker.person.firstName(),
+        last_name: faker.person.lastName(),
+      },
+      {
+        first_name: null,
+        last_name: faker.person.lastName(),
+        age: faker.number.int(),
+      },
+      {
+        first_name: faker.person.firstName(),
+        last_name: undefined,
+        age: faker.person.lastName(),
+      },
+    ];
+    objects.forEach(object => {
+      expect(isObjectKeysDefined(object, ["last_name", "first_name", "age"])).toBe(false);
+    });
+  });
+
+  // it('should return true if trying to duplicate a unique key', async () => {
+  //   const phoneNumber = faker.phone.number();
+  //   const data = [
+  //     {
+  //       phone_number: phoneNumber
+  //     }
+  //   ];
+  //   const obj = {
+  //     phone_number: phoneNumber,
+  //   }
+  //   expect(isObjectKeysUniques(data, obj, ["phone_number"])).toBe(true);
+  // });
 });
