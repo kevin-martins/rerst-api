a8dd1c89af5f64d4474d91ecfcccbd9250cf1dab
Kevin <kev.martinsvieira@gmail.com>
2023-11-21 14:44:57 +0100

    fix: remove unecessary checks

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,12 +4,10 @@ const userSchema = new mongoose.Schema({
   pass_id: String,
   first_name: {
     type: String,
-    min: [2, "Error: first name should at least be of size 2"],
     required: [true, "Error: first name is required"],
   },
   last_name: {
     type: String,
-    min: [2, "Error: last name should at least be of size 2"],
     required: [true, "Error: last name is required"],
   },
   age: {
@@ -19,12 +17,11 @@ const userSchema = new mongoose.Schema({
   phone_number: {
     type: String,
     required: [true, "Error: phone number is required"],
-    unique: true
+    unique: true,
   },
   password: {
     type: String,
     required: [true, "Error: password is required"],
-    min: [8, "Error: password should at least be of size 8"]
   },
   address: String
 });
