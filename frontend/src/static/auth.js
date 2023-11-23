c5f0e8c78835856b975d28c568d0eba917eefe12
Kevin <kev.martinsvieira@gmail.com>
2023-11-23 17:52:57 +0100

    feat: userForm data sample

---
+++ frontend/src/static/auth.js
@@ -0,0 +1,7 @@
+const staticUserForm = [
+  { label: "Nom", type: "text", id: "phone_number" },
+  { label: "Age", type: "text", id: "age" },
+  { label: "Numéro de téléphone", type: "text", id: "phone_number" },
+  { label: "Numéro de téléphone", type: "text", id: "phone_number" },
+  { label: "Numéro de téléphone", type: "text", id: "phone_number" },
+]
