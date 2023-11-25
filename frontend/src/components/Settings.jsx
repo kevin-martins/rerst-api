cd03fdbcad00d3c86e56e7bd1cf7adc714c02534
kevin-martins <kev.martinsvieira@gmail.com>
2023-11-25 20:36:38 +0100

    style: enhance settings button UI

--- frontend/src/components/Settings.jsx
+++ frontend/src/components/Settings.jsx
@@ -5,18 +5,27 @@ const Settings = ({ setIsLogged }) => {
   const [activated, setActivated] = useState(false);
 
   return (
-    <div className='relative ml-auto my-auto px-3'>
+    <div className='relative ml-auto my-auto'>
       <button
         onClick={() => setActivated(prev => !prev)}
+        className='bg-blue-500 px-5 py-2 rounded hover:bg-blue-600'
       >
         Settings
       </button>
       {activated &&
-        <div className='absolute w-44 flex flex-col bg-gray-900 border-2 rounded-lg border-yellow-600'>
-          <Link to="/profile">
-            profil
+        <div className='absolute top-14 px-5 py-2 flex flex-col bg-gray-900 rounded-lg'>
+          <Link
+            to="/profile"
+            className='hover:text-gray-300'
+          >
+            Profil
           </Link>
-          <button className='' onClick={() => setIsLogged(false)}>Log off</button>
+          <button
+            className='hover:text-gray-300'
+            onClick={() => setIsLogged(false)}
+          >
+            Déconnexion
+          </button>
         </div>
       }
     </div>
