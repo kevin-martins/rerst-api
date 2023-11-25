a1b8f118fc6219c9c4bed6132815141010e26d67
kevin-martins <kev.martinsvieira@gmail.com>
2023-11-25 18:44:51 +0100

    feat: add 403 error is user does not have the requirements

--- frontend/src/pages/PlaceId.jsx
+++ frontend/src/pages/PlaceId.jsx
@@ -1,33 +1,51 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import Loading from '../components/Loading'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Loading from '../components/Loading';
+import axios from 'axios';
+import Unauthorized from './Unauthorized';
 
-const PlaceId = () => {
+const PlaceId = ({ user }) => {
   const { id } = useParams()
   const [isLoading, setIsLoading] = useState(true)
-  const [place, setPlace] = useState(null)
+  const [place, setPlace] = useState({})
+  const [unautorized, setUnautorized] = useState(false);
 
   useEffect(() => {
-    fetch(`http://localhost:8080/places/${id}`)
-    .then((res) => res.json())
-      .then((res) => {
-        setPlace(res);
-        setIsLoading(false)
+    setIsLoading(true);
+    axios
+      .post(`http://localhost:8080/users/${user._id}/access`, {
+        placeId: id
       })
-      .catch((error) => console.log(error));
-  })
+      .then((res) => {
+        console.log(res)
+        if (res.status === 200) {
+          setPlace(res.data);
+        }
+      })
+      .catch((err) => {
+        if (err.response.status === 403) {
+          setUnautorized(true);
+        }
+      })
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  if (isLoading) {
+    return <Loading />
+  }
+
+  if (unautorized) {
+    return alert("Vous n'etes pas autorisé sur cette page");
+  }
 
   return (
     <div className='text-white'>
-      {isLoading
-        ? <Loading />
-        : <div className='grid grid-rows-4'>
-            <p>adresse actuelle: {place.address}</p>
-            <p>numéro de téléphone: {place.phone_number}</p>
-            <p>niveau du pass minimum requis: {place.required_pass_level}</p>
-            <p>age minimum requis: {place.required_age_level}</p>
-          </div>
-      }
+      <div className='grid grid-rows-4'>
+        <p>adresse actuelle: {place.address}</p>
+        <p>numéro de téléphone: {place.phone_number}</p>
+        <p>niveau du pass minimum requis: {place.required_pass_level}</p>
+        <p>age minimum requis: {place.required_age_level}</p>
+      </div>
     </div>
   )
 }
