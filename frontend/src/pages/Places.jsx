c0cecb3fe9b1eabbd5bde6d604af969af17f4d5c
kevin-martins <kev.martinsvieira@gmail.com>
2023-11-25 20:39:27 +0100

    feat: paste the Card component content into Place page

--- frontend/src/pages/Places.jsx
+++ frontend/src/pages/Places.jsx
@@ -1,36 +1,65 @@
 import React, { useEffect, useState } from 'react'
 import Loading from '../components/Loading';
-import Card from '../components/Card';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
-const Places = () => {
+const Places = ({ user }) => {
   const [isLoading, setIsLoading] = useState(true);
   const [places, setPlaces] = useState([]);
+  const [passLevel, setPassLevel] = useState();
 
   useEffect(() => {
-    fetch('http://localhost:8080/places')
-      .then((res) => res.json())
-      .then((res) => {
-        console.log(res)
-        setPlaces(res);
-        setIsLoading(false);
+    setIsLoading(true);
+    axios
+      .get('http://localhost:8080/places')
+      .then(res => {
+        if (res.status === 200) {
+          setPlaces(res.data);
+        }
       })
-      .catch((error) => console.log(error));
-  })
+      .catch((err) => console.log(err))
+      .finally(setIsLoading(false));
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8080/passes/${user.pass_id}`)
+      .then(res => {
+        if (res.status === 200) {
+          setPassLevel(res.data.level);
+        }
+      })
+      .catch((err) => console.log(err))
+      .finally(setIsLoading(false));
+  },  []);
+
+  if (isLoading) {
+    return <Loading />
+  }
 
   return (
     <>
-      {isLoading
-        ? <Loading />
-        : <ul className='flex flex-wrap gap-5'>
-            {places.map(place => (
-              <Card
-                key={place._id}
-                link={`/places/${place._id}`}
-                text="Voir la place"
-              />
-            ))}
-          </ul>
-        }
+      <p className='text-center text-white mb-5'>Age: {user.age}, Niveau du pass: {passLevel}</p>
+      <ul className='flex flex-wrap gap-5'>
+        {places.map(place => (
+          <li className='w-96 bg-slate-900 mx-auto text-white rounded-lg border-2 border-slate-900'>
+            <div className='grid grid-cols-2 gap-x-2'>
+              <p className='ml-auto'>age requis: </p>
+              <p className={user.age < place.required_age_level ? 'text-red-500' : 'text-green-500'}>{place.required_age_level}</p>
+              <p className='ml-auto'>niveau de pass requis: </p>
+              <p className={passLevel < place.required_pass_level ? 'text-red-500' : 'text-green-500'}>{place.required_pass_level}</p>
+            </div>
+            <div className='w-full h-28 grid place-items-center'>
+              <Link
+                to={`/places/${place._id}`}
+                className='m-auto bg-blue-500 rounded hover:bg-blue-600 px-5 py-2'
+              >
+                Voir la place
+              </Link>
+            </div>
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
