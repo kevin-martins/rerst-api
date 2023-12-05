585969d00522d5e368d7fc70124f6b0149722bd5
kevin-martins <kev.martinsvieira@gmail.com>
2023-12-05 17:05:48 +0100

    feat: start jwt connexion middleware

--- backend/routes/middleware.js
+++ backend/routes/middleware.js
@@ -1,35 +1,10 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
 
-// exports.auth = async (req, res, next) => {
-//   try {
-//     const { phoneNumber, password } = req.body;
-//     console.log(phoneNumber)
-//     const user = await User.findOne({ phone_number: phoneNumber });
-//     const pwd = await bcrypt.compare(password, user.password)
-//     if (!user || !(pwd)) {
-//       console.log("error")
-//       return res.status(401).json({ message: 'Error: invalid phone number or password' });
-//     }
-
-//     console.log(user)
-//     console.log(user.password, password, pwd)
-//     next();
-//   } catch (err) {
-//     res.status(500).json({ error: err.message });
-//   }
-// }
-
 // app.post('/login', (req, res) => {
-//   const { username, password } = req.body;
-//   const user = users.find((u) => u.username === username && u.password === password);
-//   if (user) {
-//     // Generate JWT token with user ID and any other data
-//     const token = jwt.sign({ id: user.id, username: user.username }, JWT_SECRET, {
-//       expiresIn: '1h' // Token expiration time
-//     });
-//     res.json({ token });
-//   } else {
-//     res.status(401).json({ message: 'Invalid credentials' });
-//   }
-// })
+//   jwt.verify(token, 'shhhhh', function(err, decoded) {
+//     if (err) {
+     
+//     }
+//   });
+// });
